25b876494d1cd9dde399fd08631bd4cf9abc6ef4
Federico-Bargosi <bargossifede98@gmail.com>
2025-01-25 16:20:01 +0100

    feat: rotta index funzionante

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,8 +1,24 @@
+const dbConnection = require("../data/dbConnection")
+
 const index = (req, res) => {
-    res.json({
-        message: "index movies"
-    })
-}
+    const sql = "SELECT * FROM `movies`";
+
+    dbConnection.query(sql, (err, movies) => {
+        if (err) {
+            const resObj = {
+                message: "Errore interno del server"
+            }
+            if (process.env.ENVIRONMENT === "development") {
+                resObj.detail = err.stack;
+            }
+            return res.status(500).json(resObj);
+        }
+        return res.status(200).json({
+            status: "succes",
+            data: movies,
+        });
+    });
+};
 
 const show = (req, res) => {
     res.json({
